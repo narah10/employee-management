9bed8d7b281e53cf960be2c1730bff2da180385f
narah10 <kwa18001@byui.edu>
2023-10-14 12:27:42 -0600

    Updated to hrrps

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Description',
   },
   host: 'narah-employee-management.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';

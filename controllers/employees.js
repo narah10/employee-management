1dcb69c2affd249aa598703fe60e9c19a24fce14
narah10 <kwa18001@byui.edu>
2023-10-21 21:42:44 -0600

    assignment 6

--- controllers/employees.js
+++ controllers/employees.js
@@ -3,16 +3,41 @@ const ObjectId = require('mongodb').ObjectId;
 
 //get all employees info
 const getAll = async (req, res) => {
+  try {
     const result = await mongodb.getDb().db('employees').collection('employees').find();
-    result.toArray().then((lists) => {
-      console.log(lists)
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists); 
-    });
-  };
+    const lists = await result.toArray();
+    console.log(lists);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  } catch (error) {
+    console.error("Error while fetching employees:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+//get single employee with id
+const getSingle = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to find a contact.');
+  }
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db('employees')
+    .collection('employees')
+    .find({ _id: userId });
+  result.toArray().then((lists) => {
+    console.log(lists)
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
+};
 
 //updating employee with id
 const updateEmployee = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to find a contact.');
+  }
     const userId = new ObjectId(req.params.id);
     const employee = {
         firstName: req.body.firstName,
@@ -23,10 +48,10 @@ const updateEmployee = async (req, res) => {
         dob: req.body.dob
       }
     const result = await mongodb.getDb().db('employees').collection('employees').replaceOne({ _id: userId }, employee);
-    if (result.acknowledged){
-      res.status(204).json(result);
+    if (result.modifiedCount > 0){
+      res.status(204).send();
     } else {
-      res.status(500).json(result.error);
+      res.status(500).json(result.error || 'Some error occurred while updating the employee information');
     }
   
   }
@@ -51,4 +76,15 @@ const createEmployee = async (req, res) => {
     }
   }
 
-module.exports= {getAll, createEmployee, updateEmployee}
+//deleting employee
+const deleteEmployee = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('employees').collection('employees').deleteOne({ _id: userId }, true);
+  if (result.deletedCount > 0){
+    res.status(200).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the employee');
+  }
+}
+
+module.exports= {getAll, createEmployee, updateEmployee, getSingle, deleteEmployee}
--- routes/employees.js
+++ routes/employees.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const employeesController = require('../controllers/employees');
+const validation = require('../middleware/validate')
 
 router.get('/', employeesController.getAll);
-router.post('/', employeesController.createEmployee);
-router.put('/:id', employeesController.updateEmployee);
+router.get('/:id', employeesController.getSingle);
+router.post('/', validation.saveEmployee, employeesController.createEmployee);
+router.put('/:id', validation.saveEmployee, employeesController.updateEmployee);
+router.delete('/:id', employeesController.deleteEmployee)
 
 module.exports = router;

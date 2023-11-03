63327574073bf5ec0a3bbbe75b21930ce878b497
narah10 <kwa18001@byui.edu>
2023-11-02 21:11:27 -0600

    Finished adding the other routes

--- controllers/department.js
+++ controllers/department.js
@@ -33,21 +33,63 @@ const getSingle = async (req, res) => {
     });
   };
 
-  //create department
-//   const createDepartment = async (req, res) => {
-//     const department = {
-//       departmentName: req.body.departmentName,
-//       departmentHead: req.body.departmentHead,
-//       departmentBudget: req.body.departmentBudget,
-//     }
-//     const result = await mongodb.getDb().db('employees').collection('departments').insertOne(department);
-//     console.log(result)
-//     if (result.acknowledged){
-//       res.status(201).json(result);
-//     } else {
-//       res.status(500).json(result.error);
-//     }
-//   }
+  const createDepartment = async (req, res) => {
+    try {
+      console.log('Request Body:', req.body);
+      const newDepartment = {
+        departmentName: req.body.departmentName,
+        departmentHead: req.body.departmentHead,
+        departmentBudget: req.body.departmentBudget,
+      };
+  
+      const result = await mongodb.getDb().db('employees').collection('departments').insertOne(newDepartment);
+      console.log(result);
+  
+      if (result.acknowledged) {
+        res.status(201).json(result);
+      } else {
+        res.status(500).json(result.error);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  //updating department with id
+const updateDepartment = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to find a department.');
+  }
+    const departmentId = new ObjectId(req.params.id);
+    const newDepartment = {
+      departmentName: req.body.departmentName,
+      departmentHead: req.body.departmentHead,
+      departmentBudget: req.body.departmentBudget,
+    };
+    const result = await mongodb.getDb().db('employees').collection('departments').replaceOne({ _id: departmentId }, newDepartment);
+    if (result.modifiedCount > 0){
+      res.status(204).send();
+    } else {
+      res.status(500).json(result.error || 'Some error occurred while updating the department information');
+    }
+  
+  }
+
+  //deleting department
+const deleteDepartment = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid department id to delete a department info.');
+  }
+  const departmentId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('employees').collection('departments').deleteOne({ _id: departmentId }, true);
+  if (result.deletedCount > 0){
+    res.status(200).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the department');
+  }
+}
 
 
-module.exports={getAll, getSingle}
+  
+module.exports={getAll, getSingle, createDepartment, updateDepartment, deleteDepartment}

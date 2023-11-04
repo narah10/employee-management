a8c3cecaad51206d99acdcb026d629b5070b4d87
narah10 <kwa18001@byui.edu>
2023-11-04 17:20:27 -0600

    data validation

--- routes/department.js
+++ routes/department.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// const validation = require('../middleware/validate')
+const validation = require('../middleware/validate')
 const departmentController = require('../controllers/department')
 
-// router.get('/', employeesController.getAll);
 router.get('/', departmentController.getAll)
 router.get('/:id', departmentController.getSingle)
-router.post('/', departmentController.createDepartment)
-router.put('/:id', departmentController.updateDepartment)
+router.post('/', validation.saveDepartment, departmentController.createDepartment)
+router.put('/:id', validation.saveDepartment, departmentController.updateDepartment)
 router.delete('/:id', departmentController.deleteDepartment)
 
 

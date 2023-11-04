a63edf43f35488fb5fcbaff276a0609372a496eb
narah10 <kwa18001@byui.edu>
2023-11-04 00:06:53 -0600

    hope for a fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT;
 const config = {
   authRequired: true, // Set to true to enforce authentication for all routes
   auth0Logout: true,
-  secret: process.env.SESSION_SECRET,
+  secret: '9CbRVNLyNw-pDm8sYfeMY9xV0PPUZyYKVrM2fYSbSFjoeaGSz3pC0XlMX7IZ3CjF',
   baseURL: process.env.BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASEURL
